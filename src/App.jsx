2fa5d38236f9a2e19a53e0b5ec94cac33131bc37
vaanskii <giorgivanadze03@gmail.com>
2024-08-21 21:24:08 +0400

    Add SoundSection component to highlight the new sound system
    - Implemented SoundSection component to present the new sound system.
    - Included title, description, and pricing details for the sound system.
    - Implement Jumbotron and SoundSection to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
 
 function App() {
 
   return (
     <div className="App">
       <Nav />
+      <Jumbotron />
+      <SoundSection />
     </div>
   );
 }
