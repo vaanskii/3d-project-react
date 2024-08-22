ef21dc5b32555801f7a6c57bcbb301a669bfc3c1
vaanskii <giorgivanadze03@gmail.com>
2024-08-22 08:49:02 +0400

    Added DisplaySection component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Jumbotron />
       <SoundSection />
+      <DisplaySection />
     </div>
   );
 }
