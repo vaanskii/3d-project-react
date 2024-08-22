61f247f93d2778e34996d99a8879149e76ed8df4
vaanskii <giorgivanadze03@gmail.com>
2024-08-22 08:43:36 +0400

    Add smooth scroll to SoundSection in Jumbotron component

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,16 @@ import Iphone from '../assets/images/iphone-14.jpg'
 import HoldingIphone from '../assets/images/iphone-hand.png'
 
 function Jumbotron() {
+
+    const handleLearnMore = () => {
+        const element = document.querySelector('.sound-section');
+        window.scrollTo({
+            top: element?.getBoundingClientRect().top,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }
+
     return (
         <div className="jumbotron-section wrapper">
             <h2 className="title">New</h2>
@@ -16,7 +26,7 @@ function Jumbotron() {
                     <button className='button'>Buy</button>
                 </li>
                 <li>
-                    <a href="#" className='link'>Learn more</a>
+                    <a href="#" onClick={handleLearnMore} className='link'>Learn more</a>
                 </li>
             </ul>
             <img src={HoldingIphone} alt="iphone" className="iphone-img" />
